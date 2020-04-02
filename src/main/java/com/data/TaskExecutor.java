f81a3e47ecc8f4c5ef062475dfa196b4636577fc
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2020-04-02 06:26:12 +0530

    Upload files.txt to hadoop from code

--- src/main/java/com/data/TaskExecutor.java
+++ src/main/java/com/data/TaskExecutor.java
@@ -1,5 +1,12 @@
 package com.data;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
@@ -15,19 +22,50 @@ import org.apache.hadoop.util.ToolRunner;
 
 import com.data.mappers.LinkMapper;
 import com.data.reducers.LinkReducer;
+import com.data.utils.ZipExtractor;
+import com.data.utils.ZipFileDownloader;
 
 /**
  * The TaskExecutor. It holds implementation to execute the map-reduce task.
  */
 public class TaskExecutor extends Configured implements Tool {
 
+	public void downloadAndProcessZip(String zipURL) throws IOException {
+
+		String zipFileName = ZipFileDownloader.download(new URL(zipURL));
+		ZipExtractor.extract(Paths.get(zipFileName));
+
+		// Preparing files.txt
+		try (FileOutputStream fileOutputStream = new FileOutputStream("files.txt");
+				Stream<java.nio.file.Path> paths = Files
+						.list(Paths.get(zipFileName.substring(0, zipFileName.lastIndexOf(".zip"))));) {
+			paths.filter(path -> path.toFile().isFile()).forEach(path -> {
+				try {
+					fileOutputStream.write(path.toFile().getAbsolutePath().getBytes());
+					fileOutputStream.write("\n".getBytes());
+				} catch (IOException ioException) {
+					System.err.println("Error adding the file path in files.txt");
+				}
+			});
+		}
+	}
+
+	public void uploadMetadataToHadoop() throws IOException {
+
+		Runtime.getRuntime().exec("hdfs dfs -rm /files.txt");
+		Runtime.getRuntime().exec("hdfs dfs -put files.txt /");
+	}
+
 	public int run(String[] args) throws Exception {
 
-		if (args.length != 2) { // Checking for command line arguments
+		if (args.length != 1) { // Checking for command line arguments
 			System.err.printf("Invalid arguments count");
 			ToolRunner.printGenericCommandUsage(System.err);
 			return -1;
 		}
+
+		this.downloadAndProcessZip(args[0]);
+		this.uploadMetadataToHadoop();
 
 		JobConf conf = new JobConf(TaskExecutor.class);
 		conf.setJobName("Modified Page Rank");
@@ -39,8 +77,8 @@ public class TaskExecutor extends Configured implements Tool {
 		conf.setReducerClass(LinkReducer.class);
 		conf.setInputFormat(TextInputFormat.class);
 		conf.setOutputFormat(TextOutputFormat.class);
-		FileInputFormat.setInputPaths(conf, new Path(args[0]));
-		FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+		FileInputFormat.setInputPaths(conf, new Path("/files.txt"));
+		FileOutputFormat.setOutputPath(conf, new Path("/modified-pagerank"));
 
 		try {
 			JobClient.runJob(conf);
