b08589ac0ba9b0ea1729855324005cdbb0506954
himanshu-orsolutions <himanshu.optiroutesolutions@gmail.com>
2020-04-12 21:44:38 +0530

    modify stale link checker

--- src/main/java/com/data/utils/StaleLinkChecker.java
+++ src/main/java/com/data/utils/StaleLinkChecker.java
@@ -1,13 +1,6 @@
 package com.data.utils;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.HashSet;
-
 public class StaleLinkChecker {
-
-	private static HashSet<String> staleLinks = new HashSet<>();
-	private static HashSet<String> activeLinks = new HashSet<>();
 
 	/**
 	 * Checks if the URL is stale or not
@@ -17,18 +10,6 @@ public class StaleLinkChecker {
 	 */
 	public static boolean check(String url) {
 
-		if (staleLinks.contains(url)) {
-			return true;
-		} else if (activeLinks.contains(url)) {
-			return false;
-		} else {
-			try {
-				URL httpURL = new URL(url);
-				httpURL.openConnection().connect();
-			} catch (IOException ioException) {
-				return true;
-			}
-			return false;
-		}
+		return true;
 	}
 }
